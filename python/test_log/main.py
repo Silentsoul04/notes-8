67ce12307a7e94ddb923d0cbda023dc0252827dd
yizishao <yinzishao@youmi.net>
2020-06-19 09:25:22 +0800

    参考链接

--- python/test_log/main.py
+++ python/test_log/main.py
@@ -7,7 +7,7 @@ import foo
 logging.config.dictConfig({
     'version': 1,
     # 是否禁用之前logging配置之前，已经存在的logger，默认为True
-    # 因为除非在配置中明确命名它们（或一个上级节点中），否则它将导致在 fileConfig() 调用之前存在的任何非 root 记录器被禁用。
+    # 因为除非在配置中明确命名它们（或一个上级节点中，不能被root捕获到），否则它将导致在 fileConfig() 调用之前存在的任何非 root 记录器被禁用。
     # 并刷新为变更为当前
     'disable_existing_loggers': True,  # this fixes the problem
     'formatters': {
