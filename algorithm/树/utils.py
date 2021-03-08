0dc172d3bb8344c198a570f201c542a734049320
yinzishao <370953598@qq.com>
2021-03-08 23:26:48 +0800

    最大深度

---
+++ algorithm/树/utils.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def init_tree(input):
+    length = len(input)
+    nums = [TreeNode(val=i) if i else None for i in input]
+    for i in range(length):
+        left = 2 * i + 1
+        right = 2 * i + 2
+        if left < length:
+            nums[i].left = nums[left]
+        if right < length:
+            nums[i].right = nums[right]
+
+    return nums[0]
+
+
+if __name__ == "__main__":
+    a = init_tree([3, 9, 20, None, None, 15, 7])
+    print(a)
